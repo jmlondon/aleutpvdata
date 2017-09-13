a96f2d777490b8e873e66c2e648e09ef52acf4f3
Josh London <josh.london@noaa.gov>
2017-09-12 17:14:36 -0700

    INITIAL COMMIT

---
+++ R/data.R
@@ -0,0 +1,59 @@
+#' Location Data (processed)
+#'
+#' Processed location data from the satellite telemetry deployments on
+#' harbor seals in the Aleutian Islands, Alaska from 2014-2017
+#'
+#' @docType data
+#'
+#' @usage data(tbl_locs)
+#'
+#' @format An object of class \code{"data.frame"};
+#'
+#' @keywords datasets
+#'
+"tbl_locs"
+
+#' Percent Timeline Data (processed)
+#'
+#' Processed percent timeline data from satellite telemetry deployments on
+#' harbor seals in the Aleutian Islands, Alaska from 2014-2017
+#'
+#' @docType data
+#'
+#' @usage data(tbl_percent)
+#'
+#' @format An object of class \code{"data.frame"};
+#'
+#' @keywords datasets
+#'
+"tbl_percent"
+
+#' Dive Behavior Data (processed)
+#'
+#' Processed dive behavior data from satellite telemetry deployments on
+#' harbor seals in the Aleutian Islands, Alaska from 2014-2017
+#'
+#' @docType data
+#'
+#' @usage data(tbl_behav_dive)
+#'
+#' @format An object of class \code{"data.frame"};
+#'
+#' @keywords datasets
+#'
+"tbl_behav_dive"
+
+#' Surface Behavior Data (processed)
+#'
+#' Processed surfaced behavior data from satellite telemetry deployments on
+#' harbor seals in the Aleutian Islands, Alaska from 2014-2017
+#'
+#' @docType data
+#'
+#' @usage data(tbl_behav_surf)
+#'
+#' @format An object of class \code{"data.frame"};
+#'
+#' @keywords datasets
+#'
+"tbl_behav_surf"
